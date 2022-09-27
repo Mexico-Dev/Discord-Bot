3d3c9cdca42ae4324c7c4c2a77a7919266771841
Gael-devv <69738055+Gael-devv@users.noreply.github.com>
2022-09-27 00:17:31 -0500

    changes to take better advantage of the json encoder

--- discord-bot/cogs/admin.py
+++ discord-bot/cogs/admin.py
@@ -4,70 +4,97 @@ import utils
 import json
 import re 
 
-from typing import Any, Union
+from typing import Any, Union, Optional
 
 
 class Announcement(commands.Converter):
-    def __init__(self, message_id: Union[int, str] = None, *, channel_id: Union[int, str] = None, embed: discord.Embed = None, mentions: list = None) -> None:
-        self.message_id = message_id
-        self.channel_id = channel_id
-        self.embed = embed
-        self.mentions = mentions
-        self.regex = re.compile(r"```(.*)\n*([\s\S]*?)\n*```", flags=re.MULTILINE)
-
-    async def convert(self, ctx: utils.Context, argument: str):
-        data: dict = json.loads(self.regex.findall(argument)[0][1])
-        if not data:
-            raise Exception("missing data")
+    regex = re.compile(r"```(.*)\n*([\s\S]*?)\n*```", flags=re.MULTILINE)
+    
+    def __init__(self) -> None:
+        self.message_id: Optional[Union[str, int]] = None
+        self.channel_id: Optional[Union[str, int]] = None
+        self.mentions: list[str] = []
         
-        mentions = data.pop("mentions", [])
-        if mentions:
-            mentions = [
-                ctx.guild.get_role(role)
-                if role != "everyone" else ctx.guild.default_role
-                for role in mentions 
-            ]
+        self.title: str = None
+        self.description: Optional[str] = None
+        self.thumbnail: Optional[str] = None
+        self.image: Optional[str] = None
+        self.fields: list[dict[str, str]] = []
+        self.color: Optional[discord.Colour] = discord.Colour.default()
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        self = cls.__new__(cls)
+        
+        self.message_id = data.get("message_id")
+        self.channel_id = data.get("channel_id")
+        self.mentions = data.get("mentions", [])
         
         if color := data.get("color"):
-            data["color"] = discord.Colour.from_rgb(*color)
+            self.color = discord.Colour.from_rgb(*color)
         else:
-            data["color"] = discord.Colour.default()
+            self.color = discord.Colour.default()
         
-        thumbnail = data.pop("thumbnail", None)
-        image = data.pop("image", None)
-        fields = data.pop("fields", [])
+        self.title = data["title"]
+        self.description = data.get("description")
+        self.thumbnail = data.get("thumbnail")
+        self.image = data.get("image")
+        self.fields = data.get("fields", [])
         
-        # Embed
-        embed = discord.Embed(timestamp=ctx.utcnow(), **data).set_footer(text=f"Anuncio enviado por {ctx.author}", icon_url=ctx.author.avatar.url)
+        return self
+
+    async def convert(self, ctx: utils.Context, argument: str):
+        announcement = json.loads(self.regex.findall(argument)[0][1], object_hook=announcement_decoder)
+        if not isinstance(announcement, Announcement):
+            raise Exception("error in the format")
         
-        if thumbnail is not None:
-            embed.set_thumbnail(url=(thumbnail, ctx.guild.icon.url)[thumbnail == "guild"])
+        mentions = [
+            ctx.guild.get_role(role).mention
+            if role != "everyone" else "@everyone"
+            for role in announcement.mentions 
+        ]
+        announcement.mentions = mentions
+        
+        return announcement
+    
+    def to_embed(self, *, author: discord.Member, guild: discord.Guild) -> discord.Embed:
+        embed = discord.Embed(color=self.color, title=self.title, description=self.description, timestamp=discord.utils.utcnow())
+        embed.set_footer(text=f"Anuncio enviado por {author}", icon_url=author.avatar.url)
+        
+        if self.thumbnail is not None:
+            embed.set_thumbnail(url=(self.thumbnail, guild.icon.url)[self.thumbnail == "guild"])
             
-        if image is not None:
-            embed.set_image(image)
+        if self.image is not None:
+            embed.set_image(self.image)
             
-        for field in fields:
+        for field in self.fields:
             embed.add_field(**field)
         
-        ad = Announcement(
-            0, channel_id=1, embed=embed, mentions=mentions
-        )
-        
-        return ad
+        return embed
     
     def to_dict(self) -> dict:
         payload = {
             "message_id": self.message_id,
             "channel_id": self.channel_id,
-            "embed": self.embed.to_dict(),
+            "mentions": self.mentions,
+            "title": self.title
         }
-
-        payload["mentions"] = [
-            role.id 
-            if isinstance(role, discord.Role) else role
-            for role in self.mentions
-        ]
         
+        if self.color:
+            payload["color"] = self.color.to_rgb()
+        
+        if self.description:
+            payload["description"] = self.description
+            
+        if self.thumbnail:
+            payload["thumbnail"] = self.thumbnail
+            
+        if self.image:
+            payload["image"] = self.image
+        
+        if self.fields:
+            payload["fields"] = self.fields
+            
         return payload
 
 
@@ -81,10 +108,8 @@ class AnnouncementEncoder(json.JSONEncoder):
 
 def announcement_decoder(obj: Any) -> Any:
     if isinstance(obj, dict):
-        if "message_id" in obj:            
-            return Announcement(
-                obj["message_id"], channel_id=obj["channel_id"], embed=discord.Embed.from_dict(obj["embed"]), mentions=obj["mentions"]
-            )
+        if "title" in obj:
+            return Announcement.from_dict(obj)
     
     return obj
 
@@ -95,7 +120,6 @@ class Admin(utils.Cog):
         self.announces: utils.Config[Announcement] = utils.Config(
             "announces.json", object_hook=announcement_decoder, encoder=AnnouncementEncoder
         )
-        self.regex = re.compile(r"```(.*)\n*([\s\S]*?)\n*```", flags=re.MULTILINE)
         
     def cog_check(self, ctx: utils.Context) -> bool:
         return ctx.author.guild_permissions.administrator
@@ -103,53 +127,33 @@ class Admin(utils.Cog):
     @commands.command()
     async def announce(self, ctx: utils.Context, channel: discord.TextChannel, *, announcement: Announcement):
         """Envía un anuncio a un canal especifico"""
-        msg = await channel.send(" ".join(map(str, announcement.mentions)), embed=announcement.embed)
+        msg = await channel.send(" ".join(map(str, announcement.mentions)), embed=announcement.to_embed(author=ctx.author, guild=ctx.guild), allowed_mentions=discord.AllowedMentions(roles=True, everyone=True))
         announcement.message_id = msg.id
         announcement.channel_id = channel.id
         
         await self.announces.put(ctx.message.id, announcement)
-        await ctx.reply("Anuncio enviado!", allowed_mentions=discord.AllowedMentions(roles=True, everyone=True))
-        
+        await ctx.reply("Anuncio enviado!")
+            
     @commands.Cog.listener()
-    async def on_message_edit(self, before, after):
-        if before.id in self.announces:
-            announcement = self.announces[before.id]
+    async def on_raw_message_edit(self, payload: discord.RawMessageUpdateEvent):
+        if payload.message_id in self.announces:
+            announcement = self.announces[payload.message_id]
             channel = await self.bot.fetch_channel(announcement.channel_id)
+            author = await channel.guild.fetch_member(payload.data["author"]["id"])
             message = await channel.fetch_message(announcement.message_id)
             
-            content = json.loads(self.regex.findall(after.content)[0][1])
-            embed = announcement.embed
-            mentions = content.pop("mentions", announcement.mentions)
+            new_announcement: Announcement = json.loads(Announcement.regex.findall(payload.data["content"])[0][1], object_hook=announcement_decoder)
+            new_announcement.channel_id = channel.id
+            new_announcement.message_id = message.id
+            mentions = [
+                channel.guild.get_role(role).mention
+                if role != "everyone" else "@everyone"
+                for role in new_announcement.mentions 
+            ]
+            new_announcement.mentions = mentions
             
-            # Update the embed
-            if title := content.get("title"): 
-                embed.title = title
-            
-            if description := content.get("description"): 
-                embed.description = description
-            
-            if url := content.get("url"): 
-                embed.url = url
-            
-            if color := content.get("color"): 
-                embed.colour = discord.Colour.from_rgb(*color)
-            
-            if image := content.get("image"): 
-                embed.set_image(url=image)
-            
-            if fields := content.get("fields"):
-                embed.clear_fields() 
-                [embed.add_field(**field) for field in fields]
-            
-            if thumbnail := content.get("thumbnail"): 
-                embed.set_thumbnail(
-                    url=thumbnail
-                    if thumbnail != "guild" else 
-                    channel.guild.icon.url
-                )
-            
-            await self.announces.put(before.id, announcement)
-            await message.edit(content=" ".join(map(str, mentions)), embed=embed)
+            await self.announces.put(payload.message_id, new_announcement)
+            await message.edit(content=" ".join(map(str, new_announcement.mentions)), embed=new_announcement.to_embed(author=author, guild=channel.guild), allowed_mentions=discord.AllowedMentions(roles=True, everyone=True))
 
 
 async def setup(bot):
